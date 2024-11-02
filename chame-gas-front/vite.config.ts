0f520d32ea806d287422457876b7af36b472e4fb
kaiostavares <kaiosandek@outlook.com>
2024-11-02 13:43:55 -0300

    Prepare for GitHub Pages deployment

--- chame-gas-front/vite.config.ts
+++ chame-gas-front/vite.config.ts
@@ -5,12 +5,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  base: '/chame-gas/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
   }
 })
