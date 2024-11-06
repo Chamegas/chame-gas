a98d638f3702d07dbf351d59d182456f87d4bb7d
kaiostavares <kaiosandek@outlook.com>
2024-11-06 02:19:21 -0300

    chore: dockerize app with nginx

--- chame-gas-front/vite.config.ts
+++ chame-gas-front/vite.config.ts
@@ -6,7 +6,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/chame-gas/',
+  //put this when will do deploy to github pages
+  // base: '/chame-gas/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
